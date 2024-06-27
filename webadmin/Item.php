1ebc019ae235fd6e8e78f48ad622d8fb7c9b429d
nukemastah <145178126+nukemastah@users.noreply.github.com>
2024-06-27 21:02:35 +0700

    item search button

--- webadmin/Item.php
+++ webadmin/Item.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>Dashboard</title>
 
     <!-- CSS UNTUK TABEL -->
 
@@ -56,6 +56,54 @@
 </head>
 
 <body id="page-top">
+<!-- Load library ikon -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!-- Form pencarian -->
+<form class="example" action="item.php">
+    <input type="text" placeholder="Cari.." name="search">
+    <button type="submit"><i class="fa fa-search"></i></button>
+</form>
+
+<!-- CSS -->
+<style>
+    * {
+        box-sizing: border-box;
+    }
+
+    /* Gaya bidang pencarian */
+    form.example input[type=text] {
+        padding: 10px;
+        font-size: 17px;
+        border: 1px solid grey;
+        float: left;
+        width: 80%;
+        background: #f1f1f1;
+    }
+
+    /* Gaya tombol submit */
+    form.example button {
+        float: left;
+        width: 20%;
+        padding: 10px;
+        background: #2196F3;
+        color: white;
+        font-size: 17px;
+        border: 1px solid grey;
+        cursor: pointer;
+    }
+
+    form.example button:hover {
+        background: #0b7dda;
+    }
+
+    /* Membersihkan float */
+    form.example::after {
+        content: "";
+        clear: both;
+        display: table;
+    }
+</style>
 
     <!-- Page Wrapper -->
     <div id="wrapper">
